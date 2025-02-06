c2415cebe93961c44ef7ec24662acc1c68051dd2
PineappleLord2020 <mul20002@byui.edu>
2025-02-06 15:54:28 -0700

    AAAAAAAHHHHHHHHH

--- controllers/temple.js
+++ controllers/temple.js
@@ -1,8 +1,8 @@
 const db = require('../models');
 const Temple = db.temples;
 
-const apiKey =
-  'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N';
+//const apiKey =
+  //'Ezl0961tEpx2UxTZ5v2uKFK91qdNAr5npRlMT1zLcE3Mg68Xwaj3N8Dyp1R8IvFenrVwHRllOUxF0Og00l0m9NcaYMtH6Bpgdv7N';
 
 exports.create = (req, res) => {
   // Validate request
@@ -56,7 +56,7 @@ exports.findAll = (req, res) => {
         });
       });
   //} else {
-    res.send('Invalid apiKey, please read the documentation.');
+    //res.send('Invalid apiKey, please read the documentation.');
   }
 //};
 
@@ -78,7 +78,7 @@ exports.findOne = (req, res) => {
         });
       });
   //} else {
-    res.send('Invalid apiKey, please read the documentation.');
+    //res.send('Invalid apiKey, please read the documentation.');
   }
 //};
 
--- routes/temple.js
+++ routes/temple.js
@@ -3,7 +3,7 @@ const temples = require('../controllers/temple.js');
 
 /**
  * @swagger
- * /users:
+ * /temples:
  *   get:
  *     summary: Get a list of temples
  *     description: Retrieve a list of users from the database.
@@ -13,7 +13,28 @@ const temples = require('../controllers/temple.js');
  */
 
 routes.get('/', temples.findAll);
+
+//https://swagger.io/docs/specification/v3_0/describing-parameters/
+
+/**
+ * @swagger
+ * /temples/{temple_id}:
+ *   get:
+ *     summary: Get a list of temples by the Temple ID
+ *     description: Retrieve a list of temples from the database.
+ *     parameters:
+ *      - in: path
+ *        name: temple_id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *     responses:
+ *       200:
+ *         description: Successful response with a list of users.
+ */
+
 routes.get('/:temple_id', temples.findOne);
+
 
 routes.post('/', temples.create);
 
